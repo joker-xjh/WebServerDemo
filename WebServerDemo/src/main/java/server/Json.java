80ee44b6304f33a9e84662be36f88933e87747a2
joker_xjh <1781934063@qq.com>
2017-08-29 22:30:14 +0800

    json

--- WebServerDemo/src/main/java/server/Json.java
+++ WebServerDemo/src/main/java/server/Json.java
@@ -2,10 +2,13 @@ package server;
 
 import java.util.Map;
 
+import net.sf.json.JSONObject;
+
 public class Json {
 	
-	public static void toJson(Map<String, ? extends Object> map) {
-		
+	public static String toJson(Map<String, ? extends Object> map) {
+		JSONObject json = JSONObject.fromObject(map);
+		return json.toString();
 	}
 
 }
