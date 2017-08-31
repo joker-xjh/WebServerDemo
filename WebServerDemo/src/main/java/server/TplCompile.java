1e13a3e67c341cec4aef82b10c0d9a06becc75fc
joker_xjh <1781934063@qq.com>
2017-08-31 22:33:09 +0800

    TplCompile

---
+++ WebServerDemo/src/main/java/server/TplCompile.java
@@ -0,0 +1,48 @@
+package server;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+public class TplCompile {
+	
+	public String templatePath = Options.TemplatePath;
+	
+	public void setTemplatePath(String templatePath) {
+		this.templatePath = templatePath;
+	}
+	
+	public Object run(String fileName) {
+		TemplateCompile templateCompile = new TemplateCompile();
+		int r = templateCompile.start(fileName);
+		if(r==1)
+			return null;
+		String javaFile = templatePath + templateCompile.getFilePath(fileName)+"/"+templateCompile.getfileName(fileName)+ "Template.java";
+		String classPath = getClass().getResource("/").getPath();
+		JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+		int status = javac.run(null, null, null, "-d",classPath,javaFile);
+		if(status != 0) {
+			System.out.println("没有编译成功");
+			return null;
+		}
+		
+		
+		try {
+			@SuppressWarnings("rawtypes")
+			
+			Class clazz = Class.forName("Template."+templateCompile.getFilePath(fileName).replace("/", ".")+templateCompile.getfileName(fileName)+"Template");
+			 BaseTemplate object = (BaseTemplate) clazz.newInstance();
+			 return object;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
